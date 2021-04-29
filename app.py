500e50ae749a89941f835895dbbc18d6bb89e72d
V Kartik <70521562+VKartik07@users.noreply.github.com>
2021-04-29 18:02:47 +0530

    Added Markdown and Interaction

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -43,37 +44,62 @@ air_quality_df['Area Category'] = air_quality_df['Area Category'].replace(
         'Industrial Areas', 'Sensitive Areas'],
     ['Residential and Rural Areas', 'Residential Areas', 'Industrial Area', 'Sensitive Area'])
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ############################################################### Graph 1
-State = 'Goa'
-
-Constant_Pollutants = ['Sulphur_Dioxide', 'Nitrogen_Dioxide',
-                       'Respirable_Suspended_Particulate_Matter']
-Bar_Plot_Pivot = air_quality_df.loc[(air_quality_df['State'] == State), [
-    'State', 'Sulphur_Dioxide', 'Nitrogen_Dioxide', 'Respirable_Suspended_Particulate_Matter']].groupby(by='State').mean()
-Pollutants_Numbers = Bar_Plot_Pivot.loc[State].tolist()
-
-fig = px.bar(x=Constant_Pollutants, 
-            y=Pollutants_Numbers, 
-            labels=dict(x="Pollutant", y="Average"),
-)
-
-fig.update_layout(
-    title={
-        'text': "Bar Plot - Amount of {}, {}, {} in {}".format(Constant_Pollutants[0], Constant_Pollutants[1], Constant_Pollutants[2], State),
-        'y': 0.95,
-        'x': 0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'})
 ############################################################### Graph 1
 
 app.layout = html.Div(children=[
     html.H1(children='Indian Air Quality User Dashboard', style={'textAlign':'center'}),
-
-    dcc.Graph(
-        id='bar-1-graph',
-        figure=fig
-    )
+    html.Label('States'),
+    dcc.Dropdown(
+        id='state-dropdown',
+        options=[
+            {'label':i, 'value':i} for i in air_quality_df['State'].unique()
+        ], value="Andhra Pradesh"
+        ),
+    dcc.Graph(id='bar-1-graph')
 ])
 
+@app.callback(
+    Output('bar-1-graph', 'figure'),
+    Input('state-dropdown', 'value')
+)
+def update_figure(selected_state):
+    Bar_Plot_Pivot = air_quality_df.loc[(air_quality_df['State'] == selected_state), 
+    ['State', 'Sulphur_Dioxide', 'Nitrogen_Dioxide', 'Respirable_Suspended_Particulate_Matter']].groupby(by='State').mean()
+
+    Constant_Pollutants = ['Sulphur_Dioxide', 'Nitrogen_Dioxide',
+                           'Respirable_Suspended_Particulate_Matter']
+
+    Pollutants_Numbers = Bar_Plot_Pivot.loc[selected_state].tolist()
+
+    fig = px.bar(x=Constant_Pollutants,
+                 y=Pollutants_Numbers,
+                 labels=dict(x="Pollutant", y="Average"),
+                 text=Pollutants_Numbers
+                 )
+    fig.update_layout(
+        title={
+            'text': "Bar Plot - Amount of {}, {}, {} in {}".format(Constant_Pollutants[0], Constant_Pollutants[1], Constant_Pollutants[2], selected_state),
+            'y': 0.95,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+    
+    return fig
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
